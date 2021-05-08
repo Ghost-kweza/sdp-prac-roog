12a32ad70b9f462e501413758b4c61c72d17feb0
Kwedza <you@example.com>
2021-05-08 23:38:17 +0200

    I think I have it

--- Roog/Program.cs
+++ Roog/Program.cs
@@ -6,7 +6,7 @@ namespace Roog
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("g19k7982");
             //I've added a comment
         }
     }
